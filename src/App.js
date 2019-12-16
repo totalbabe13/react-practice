837f27c4271cc5dc1f917bf533788bfa5e6f57e2
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-12-16 17:36:03 -0600

    building out book card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import BookCard from './Components/BookCard'
 
 function App() {
+  console.log(<BookCard />);
   return (
     <div className="App">
       <h1 className="layout-header">B O O K I T</h1>
+      <div className="cards-container">
+        <BookCard />
+      </div>
     </div>
   );
 }
---
+++ src/Components/BookCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './bookCard.css';
+
+class BookCard extends React.Component {
+  render() {
+    return(
+      <div className="book-card">
+      hello?
+      </div>
+    );
+  }
+};
+
+export default BookCard;
