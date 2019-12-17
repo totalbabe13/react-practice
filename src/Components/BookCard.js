2ad28c4803e53ae183a7ff21d0d4b10159ded446
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-12-16 19:12:37 -0600

    quick place holder made for book card object

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -5,7 +5,19 @@ class BookCard extends React.Component {
   render() {
     return(
       <div className="book-card">
-      hello?
+
+        <h2 className="title-tag">Sample Book Title</h2>
+        <p className="divider">______________ </p>
+        <div className="author-container">
+            <span className='author-tag'>Sample Author</span>
+        </div>
+        <div className="buttons-container">
+
+
+        </div>
+        <div className="pages-container">
+          <span className="pages-tag">335 pages</span>
+        </div>
       </div>
     );
   }
